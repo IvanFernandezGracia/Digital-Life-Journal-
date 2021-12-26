889e732046cf85bf70c6761aacd68170546cee2f
Ivan Alejandro <ivan.fernandez.g@usach.cl>
2021-12-26 18:59:42 -0300

    docs v1

---
+++ docs/precache-manifest.5a05201cb0b495b6734519d98b8544a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8187c99d22403ddd9b0c365b5daa27e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9e31401a971cfd2e673",
+    "url": "/static/css/main.26922b4e.chunk.css"
+  },
+  {
+    "revision": "3d8c52d0d8c60d78b271",
+    "url": "/static/js/2.235a95a9.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.235a95a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e31401a971cfd2e673",
+    "url": "/static/js/main.d47845c2.chunk.js"
+  },
+  {
+    "revision": "1f9b80c10cb2174ac7b3",
+    "url": "/static/js/runtime-main.8c0f0de7.js"
+  }
+]);
